6e9a42bcab4ce2def38a44209599f6a27a0162ac
curchadw <curtisdeveloper@gmail.com>
2021-08-10 07:42:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-p5m5gu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-p5m5gu)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
